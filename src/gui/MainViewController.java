0dd80008b4d166c23e8dcd6ee8fe1a8449f973ba
ReiJrMed <reijrspy@gmail.com>
2020-07-20 20:52:08 -0300

    DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 
 public class MainViewController implements Initializable{
@@ -35,7 +36,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -70,6 +71,39 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().clear(); //apaga-se todos os filhos de VBox
 			mainVBox.getChildren().add(mainMenu);//adiciona-se o Node mainMenu aos filhos do VBox (pois o inicial foi apagado pelo método clear)
 			mainVBox.getChildren().addAll(newVBox.getChildren()); //adiciona uma coleção ao VBox, no caso os filhos do newVBox;			
+		} catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2(String absoluteName) {
+		//o "synchronized" garante que este método será executado sem interrupções
+	   //em aplicações gráficas há vários processos executando, essa é uma medida de segurança	 
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			
+			VBox mainVBox = (VBox)(((ScrollPane) mainScene.getRoot()).getContent());
+			//mainScene.getRoot() acessa o primeiro item da tela principal (o ScrollPane, para acessar é preciso cast para ScrollPane)
+		   //ScrollPane.getContent() acessa os componentes do ScrollPane (o VBox, para acessar é preciso cast para VBox)
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//Para contruir uma tela dentro de outra (principal), vai ser preciso apagar os recursos do Container da tela(main) e salvá-los em Nodes;
+		   //o item que será apagado temporariamente será o MenuBar, que será salvo nesse Node
+		  //acessa-se os filhos do VBox pelo método VBox.getChildren()
+		 //acessa o filho de uma determinada posição pelo método VBox.getChildren() (o container VBox tem posições no caso verticais, inicia em zero)	
+			
+			mainVBox.getChildren().clear(); //apaga-se todos os filhos de VBox
+			mainVBox.getChildren().add(mainMenu);//adiciona-se o Node mainMenu aos filhos do VBox (pois o inicial foi apagado pelo método clear)
+			mainVBox.getChildren().addAll(newVBox.getChildren()); //adiciona uma coleção ao VBox, no caso os filhos do newVBox;
+			
+			DepartmentListController control = loader.getController();//loader.getController() captura o controler(classe responsável) do DepartmentList
+			control.setDepartmentService(new DepartmentService());
+			control.updateTableView();
+			
 		} catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
